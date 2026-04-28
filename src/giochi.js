6032c3068839eff14caab4118564a75f8b40fb4d
Cap_Dev <kekkomasterpro9@gmail.com>
2026-04-28 15:10:26 +0200

    Implementazione funzione rimuoviGioco

--- src/giochi.js
+++ src/giochi.js
@@ -13,3 +13,9 @@ const creaGioco = (titolo, piattaforma, genere, anno, studio) => {
 function aggiungiGioco(gioco) {
     collezione.push(gioco);
 }
+
+function rimuoviGioco(titoloDaCancellare) {
+    const lunghezzaIniziale = collezione.length;
+    collezione = collezione.filter(g => g.titolo.toLowerCase() !== titoloDaCancellare.toLowerCase());
+    return collezione.length < lunghezzaIniziale;
+}
